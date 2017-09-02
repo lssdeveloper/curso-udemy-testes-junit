056f9c76f98b439a4e4c97af5b18805057dab9d1
Leandro Serra <leandro.serra.10@gmail.com>
2017-09-02 16:54:35 -0300

    Agora um Teste Mais Completo (Teste 2)

--- SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
+++ SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import com.lssdeveloper.negocio.Cliente;
 import com.lssdeveloper.negocio.GerenciadoraClientes;
 
-public class GerenciadoraClientesTest_Ex1 {
+public class GerenciadoraClientesTest {
 
 	@Test
 	public void testPesquisaCliente() {
@@ -44,6 +44,32 @@ public class GerenciadoraClientesTest_Ex1 {
 		Cliente cliente2 = gerClientes.pesquisaCliente(1);
 		assertThat(cliente2.getId(), is(1));
 		assertThat(cliente2.getEmail(), is("leandro.serra@gmail.com"));
+	}
+	@Test
+	public void testRemoveClientes(){
+		//criando clientes
+		Cliente cliente001 = new Cliente(1, "Leandro Serra", 41, "leandro.serra@gmail.com", 1, true);
+		Cliente cliente002 = new Cliente(2, "Amanda Beatriz", 19, "amanda.beatriz@gmail.com", 1, true);
+		Cliente cliente003 = new Cliente(3, "Leana Serra", 13, "leana.serra@gmail.com", 1, true);
+		
+		//inserindo na lista de clientes do banco
+		List<Cliente> lstClientesDoBanco = new ArrayList<>();
+		lstClientesDoBanco.add(cliente001);
+		lstClientesDoBanco.add(cliente002);
+		
+		GerenciadoraClientes gerClientes = new GerenciadoraClientes(lstClientesDoBanco);
+		
+		assertTrue(gerClientes.removeCliente(cliente001.getId()));
+		assertFalse(gerClientes.removeCliente(cliente003.getId()));
+	
+		/*
+		 * como foi feito no curso
+		 */
+		boolean clienteRemovido = gerClientes.removeCliente(cliente002.getId());
+		assertNull(gerClientes.pesquisaCliente(cliente002.getId()));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(0));
+		assertThat(clienteRemovido, is(true));
+
 	}
 
 }
