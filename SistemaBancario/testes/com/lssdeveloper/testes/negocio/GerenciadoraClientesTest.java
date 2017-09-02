62454077077c8d54c47dc956045869b1f6c33e42
Leandro Serra <leandro.serra.10@gmail.com>
2017-09-02 18:56:59 -0300

    Como ganhar tempo e Otimizar Testes com Cenários Parecidos (Teste 7 - Aula 49)

--- SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
+++ SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
@@ -6,6 +6,8 @@ import static org.hamcrest.CoreMatchers.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.lssdeveloper.negocio.Cliente;
@@ -13,59 +15,66 @@ import com.lssdeveloper.negocio.GerenciadoraClientes;
 
 public class GerenciadoraClientesTest {
 
-	@Test
-	public void testPesquisaCliente() {
+	private Cliente cliente001;
+	private Cliente cliente002;
+	private Cliente cliente003;
+	List<Cliente> lstClientesDoBanco;
+	GerenciadoraClientes gerClientes;
+
+	@Before
+	public void setUp() {
+
+		/* ===========Montagem do Cenário========= */
+
+		// criando clientes
+		cliente001 = new Cliente(1, "Leandro Serra", 41, "leandro.serra@gmail.com", 1, true);
+		cliente002 = new Cliente(2, "Amanda Beatriz", 19, "amanda.beatriz@gmail.com", 1, true);
+		cliente003 = new Cliente(3, "Leana Serra", 13, "leana.serra@gmail.com", 1, true);
 		
-		/*===========Montagem do Cenário=========*/
-		
-		//criando clientes
-		Cliente cliente001 = new Cliente(1, "Leandro Serra", 41, "leandro.serra@gmail.com", 1, true);
-		Cliente cliente002 = new Cliente(2, "Amanda Beatriz", 19, "amanda.beatriz@gmail.com", 1, true);
-		Cliente cliente003 = new Cliente(3, "Leana Serra", 13, "leana.serra@gmail.com", 1, true);		
-		//inserindo na lista de clientes do banco
-		List<Cliente> lstClientesDoBanco = new ArrayList<>();
+		// inserindo na lista de clientes do banco
+		lstClientesDoBanco = new ArrayList<>();
 		lstClientesDoBanco.add(cliente001);
 		lstClientesDoBanco.add(cliente002);
 		
-		GerenciadoraClientes gerClientes = new GerenciadoraClientes(lstClientesDoBanco);
+		gerClientes = new GerenciadoraClientes(lstClientesDoBanco);
+		//System.out.println("Before foi executado!");
+
+	}
+
+	@Test
+	public void testPesquisaCliente() {
+
 		/*
 		 * como eu fiz
 		 */
-		
-		//cliente existente na lista de clientes do banco
+		// cliente existente na lista de clientes do banco
 		assertNotNull(gerClientes.pesquisaCliente(cliente001.getId()));
-		//cliente não existente na lista de clientes do banco		
+		// cliente não existente na lista de clientes do banco
 		assertNull(gerClientes.pesquisaCliente(cliente003.getId()));
-		//ou
-		Cliente cliente = gerClientes.pesquisaCliente(cliente002.getId());
-		assertThat(cliente.getId(), is(2));
-		
+
+
 		/*
 		 * como foi feito no curso
 		 */
-		/* ========= Execução ========*/
-		Cliente cliente2 = gerClientes.pesquisaCliente(1);
-		/* ========= Verificação ===========*/
-		assertThat(cliente2.getId(), is(1));
-		assertThat(cliente2.getEmail(), is("leandro.serra@gmail.com"));
+		/* ========= Execução ======== */
+
+		/* ========= Verificação =========== */
+		Cliente cliente = gerClientes.pesquisaCliente(cliente001.getId());
+		assertThat(cliente.getId(), is(1));
+		assertThat(cliente.getEmail(), is("leandro.serra@gmail.com"));
 	}
+
 	@Test
-	public void testRemoveClientes(){
-		//criando clientes
-		Cliente cliente001 = new Cliente(1, "Leandro Serra", 41, "leandro.serra@gmail.com", 1, true);
-		Cliente cliente002 = new Cliente(2, "Amanda Beatriz", 19, "amanda.beatriz@gmail.com", 1, true);
-		Cliente cliente003 = new Cliente(3, "Leana Serra", 13, "leana.serra@gmail.com", 1, true);
-		
-		//inserindo na lista de clientes do banco
+	public void testRemoveClientes() {
+
+		// inserindo na lista de clientes do banco
 		List<Cliente> lstClientesDoBanco = new ArrayList<>();
 		lstClientesDoBanco.add(cliente001);
 		lstClientesDoBanco.add(cliente002);
-		
-		GerenciadoraClientes gerClientes = new GerenciadoraClientes(lstClientesDoBanco);
-		
+
 		assertTrue(gerClientes.removeCliente(cliente001.getId()));
 		assertFalse(gerClientes.removeCliente(cliente003.getId()));
-	
+
 		/*
 		 * como foi feito no curso
 		 */
@@ -75,6 +84,10 @@ public class GerenciadoraClientesTest {
 		assertThat(clienteRemovido, is(true));
 
 	}
+	@After
+	public void tearDown(){
+		gerClientes.limpa();
+		//System.out.println("After foi executado!");
+	}
 
 }
-
