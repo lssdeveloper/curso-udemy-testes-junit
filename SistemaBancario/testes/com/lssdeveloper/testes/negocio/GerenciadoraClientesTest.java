c9f74660942e071be81e15324b1a389a9370ca00
Leandro Serra <leandro.serra.10@gmail.com>
2017-09-02 21:25:09 -0300

    Teste entre limites e chamada de exceções (validaIdade entre 18 e 65 anos)

--- SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
+++ SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
@@ -12,6 +12,7 @@ import org.junit.Test;
 
 import com.lssdeveloper.negocio.Cliente;
 import com.lssdeveloper.negocio.GerenciadoraClientes;
+import com.lssdeveloper.negocio.IdadeNaoPermitidaException;
 
 public class GerenciadoraClientesTest {
 
@@ -20,6 +21,9 @@ public class GerenciadoraClientesTest {
 	private Cliente cliente003;
 	List<Cliente> lstClientesDoBanco;
 	GerenciadoraClientes gerClientes;
+	private boolean ativo;
+	boolean teste;
+
 
 	@Before
 	public void setUp() {
@@ -28,35 +32,28 @@ public class GerenciadoraClientesTest {
 
 		// criando clientes
 		cliente001 = new Cliente(1, "Leandro Serra", 41, "leandro.serra@gmail.com", 1, true);
-		cliente002 = new Cliente(2, "Amanda Beatriz", 19, "amanda.beatriz@gmail.com", 1, true);
-		cliente003 = new Cliente(3, "Leana Serra", 13, "leana.serra@gmail.com", 1, true);
-		
+		cliente002 = new Cliente(2, "Amanda Beatriz", 18, "amanda.beatriz@gmail.com", 1, false);
+		cliente003 = new Cliente(3, "Astolfo Serra", 65, "astolfo.serra@gmail.com", 1, true);
+
 		// inserindo na lista de clientes do banco
 		lstClientesDoBanco = new ArrayList<>();
 		lstClientesDoBanco.add(cliente001);
 		lstClientesDoBanco.add(cliente002);
-		
+
 		gerClientes = new GerenciadoraClientes(lstClientesDoBanco);
-		//System.out.println("Before foi executado!");
+		// System.out.println("Before foi executado!");
+		ativo = gerClientes.clienteAtivo(cliente001.getId());
 
 	}
 
 	@Test
 	public void testPesquisaCliente() {
-
-		/*
-		 * como eu fiz
-		 */
 		// cliente existente na lista de clientes do banco
+		/* como eu fiz */
+
 		assertNotNull(gerClientes.pesquisaCliente(cliente001.getId()));
-		// cliente não existente na lista de clientes do banco
-		assertNull(gerClientes.pesquisaCliente(cliente003.getId()));
 
-
-		/*
-		 * como foi feito no curso
-		 */
-		/* ========= Execução ======== */
+		/* como foi feito no curso */
 
 		/* ========= Verificação =========== */
 		Cliente cliente = gerClientes.pesquisaCliente(cliente001.getId());
@@ -65,29 +62,93 @@ public class GerenciadoraClientesTest {
 	}
 
 	@Test
+	public void testPesquisaCliente_naoExistente() {
+		// cliente não existente na lista de clientes do banco
+		assertNull(gerClientes.pesquisaCliente(cliente003.getId()));
+	}
+
+	@Test
 	public void testRemoveClientes() {
 
-		// inserindo na lista de clientes do banco
-		List<Cliente> lstClientesDoBanco = new ArrayList<>();
-		lstClientesDoBanco.add(cliente001);
-		lstClientesDoBanco.add(cliente002);
-
 		assertTrue(gerClientes.removeCliente(cliente001.getId()));
-		assertFalse(gerClientes.removeCliente(cliente003.getId()));
-
+		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
 		/*
 		 * como foi feito no curso
 		 */
 		boolean clienteRemovido = gerClientes.removeCliente(cliente002.getId());
 		assertNull(gerClientes.pesquisaCliente(cliente002.getId()));
-		assertThat(gerClientes.getClientesDoBanco().size(), is(0));
 		assertThat(clienteRemovido, is(true));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(0));
 
 	}
+
+	@Test
+	public void testRemoveClientes_inexistenteNaLista() {
+		assertFalse(gerClientes.removeCliente(cliente003.getId()));
+		assertThat(gerClientes.getClientesDoBanco().size(), is(2));
+	}
+	/*
+	 * Informa se um determinado cliente está ativo ou não
+	 */
+	@Test
+	public void testCliente_Ativo(){
+
+		assertTrue(ativo);
+	}
+	@Test
+	public void testClienteAtivo_inAtivo(){
+		ativo = gerClientes.clienteAtivo(cliente002.getId());
+		assertFalse(ativo);
+	}
+	/**
+	 * Valida se a idade do cliente está dentro do intervalo permitido (18 - 65).
+	 * Teste de Limites
+	 * @throws IdadeNaoPermitidaException 
+	 */
+	@Test
+	public void testeValidaIdade_entre18E65() throws IdadeNaoPermitidaException{
+		//cliente001 = 41 anos
+		teste = gerClientes.validaIdade(cliente001.getIdade());
+		assertTrue(teste);
+	}
+	@Test
+	public void testeValidaIdade_igual18() throws IdadeNaoPermitidaException{
+		//cliente002 = 18 anos
+		teste = gerClientes.validaIdade(cliente002.getIdade());
+		assertTrue(teste);
+	}
+	@Test
+	public void testeValidaIdade_igual65() throws IdadeNaoPermitidaException{
+		//cliente003 = 65 anos
+		teste = gerClientes.validaIdade(cliente003.getIdade());
+		assertTrue(teste);
+	}	
+	@Test
+	public void testeValidaIdade_menorQ18(){
+		cliente003.setIdade(17);
+		try {
+			teste = gerClientes.validaIdade(cliente003.getIdade());
+			fail();
+		} catch (Exception e) {
+			assertThat(e.getMessage(), is(IdadeNaoPermitidaException.MSG_IDADE_INVALIDA));
+		}
+		
+	}
+	@Test
+	public void testeValidaIdade_maiorQ65(){
+		cliente003.setIdade(66);
+		try {
+			teste = gerClientes.validaIdade(cliente003.getIdade());
+			fail();
+		} catch (Exception e) {
+			assertThat(e.getMessage(), is(IdadeNaoPermitidaException.MSG_IDADE_INVALIDA));
+		}
+		
+	}	
 	@After
-	public void tearDown(){
+	public void tearDown() {
 		gerClientes.limpa();
-		//System.out.println("After foi executado!");
+		// System.out.println("After foi executado!");
 	}
 
 }
