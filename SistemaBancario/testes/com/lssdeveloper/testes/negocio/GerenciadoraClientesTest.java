85bdefe5f8e29b00f730394d1b739533a6e3faf0
Leandro Serra <leandro.serra.10@gmail.com>
2017-09-03 22:21:22 -0300

    Inclusão de variáveis idC para receber valores do Id de Clientes. Sugestão do professor como melhor práticas, pois ajuda a visualizar o valor da variável num possível debug, além de ficar um código maslimpo e claro.

--- SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
+++ SistemaBancario/testes/com/lssdeveloper/testes/negocio/GerenciadoraClientesTest.java
@@ -19,6 +19,7 @@ public class GerenciadoraClientesTest {
 	private Cliente cliente001;
 	private Cliente cliente002;
 	private Cliente cliente003;
+	private int idC1,idC2,idC3; //Para melhor práticas sugerida pelo professor.
 	List<Cliente> lstClientesDoBanco;
 	GerenciadoraClientes gerClientes;
 	private boolean ativo;
@@ -32,8 +33,11 @@ public class GerenciadoraClientesTest {
 
 		// criando clientes
 		cliente001 = new Cliente(1, "Leandro Serra", 41, "leandro.serra@gmail.com", 1, true);
+		idC1 = cliente001.getId();
 		cliente002 = new Cliente(2, "Amanda Beatriz", 18, "amanda.beatriz@gmail.com", 1, false);
+		idC2 = cliente002.getId();
 		cliente003 = new Cliente(3, "Astolfo Serra", 65, "astolfo.serra@gmail.com", 1, true);
+		idC3 = cliente003.getId();
 
 		// inserindo na lista de clientes do banco
 		lstClientesDoBanco = new ArrayList<>();
@@ -42,7 +46,7 @@ public class GerenciadoraClientesTest {
 
 		gerClientes = new GerenciadoraClientes(lstClientesDoBanco);
 		// System.out.println("Before foi executado!");
-		ativo = gerClientes.clienteAtivo(cliente001.getId());
+		ativo = gerClientes.clienteAtivo(idC1);
 
 	}
 
@@ -51,32 +55,32 @@ public class GerenciadoraClientesTest {
 		// cliente existente na lista de clientes do banco
 		/* como eu fiz */
 
-		assertNotNull(gerClientes.pesquisaCliente(cliente001.getId()));
+		assertNotNull(gerClientes.pesquisaCliente(idC1));
 
 		/* como foi feito no curso */
 
 		/* ========= Verificação =========== */
-		Cliente cliente = gerClientes.pesquisaCliente(cliente001.getId());
-		assertThat(cliente.getId(), is(1));
+		Cliente cliente = gerClientes.pesquisaCliente(idC1);
+		assertThat(cliente.getId(), is(idC1));
 		assertThat(cliente.getEmail(), is("leandro.serra@gmail.com"));
 	}
 
 	@Test
 	public void testPesquisaCliente_naoExistente() {
 		// cliente não existente na lista de clientes do banco
-		assertNull(gerClientes.pesquisaCliente(cliente003.getId()));
+		assertNull(gerClientes.pesquisaCliente(idC3));
 	}
 
 	@Test
 	public void testRemoveClientes() {
 
-		assertTrue(gerClientes.removeCliente(cliente001.getId()));
+		assertTrue(gerClientes.removeCliente(idC1));
 		assertThat(gerClientes.getClientesDoBanco().size(), is(1));
 		/*
 		 * como foi feito no curso
 		 */
-		boolean clienteRemovido = gerClientes.removeCliente(cliente002.getId());
-		assertNull(gerClientes.pesquisaCliente(cliente002.getId()));
+		boolean clienteRemovido = gerClientes.removeCliente(idC2);
+		assertNull(gerClientes.pesquisaCliente(idC2));
 		assertThat(clienteRemovido, is(true));
 		assertThat(gerClientes.getClientesDoBanco().size(), is(0));
 
@@ -84,7 +88,7 @@ public class GerenciadoraClientesTest {
 
 	@Test
 	public void testRemoveClientes_inexistenteNaLista() {
-		assertFalse(gerClientes.removeCliente(cliente003.getId()));
+		assertFalse(gerClientes.removeCliente(idC3));
 		assertThat(gerClientes.getClientesDoBanco().size(), is(2));
 	}
 	/*
@@ -97,7 +101,7 @@ public class GerenciadoraClientesTest {
 	}
 	@Test
 	public void testClienteAtivo_inAtivo(){
-		ativo = gerClientes.clienteAtivo(cliente002.getId());
+		ativo = gerClientes.clienteAtivo(idC2);
 		assertFalse(ativo);
 	}
 	/**
@@ -107,27 +111,30 @@ public class GerenciadoraClientesTest {
 	 */
 	@Test
 	public void testeValidaIdade_entre18E65() throws IdadeNaoPermitidaException{
-		//cliente001 = 41 anos
-		teste = gerClientes.validaIdade(cliente001.getIdade());
+
+		int idade41 = cliente001.getIdade();
+		teste = gerClientes.validaIdade(idade41);
 		assertTrue(teste);
 	}
 	@Test
 	public void testeValidaIdade_igual18() throws IdadeNaoPermitidaException{
-		//cliente002 = 18 anos
-		teste = gerClientes.validaIdade(cliente002.getIdade());
+
+		int idade18 = cliente002.getIdade();
+		teste = gerClientes.validaIdade(idade18);
 		assertTrue(teste);
 	}
 	@Test
 	public void testeValidaIdade_igual65() throws IdadeNaoPermitidaException{
-		//cliente003 = 65 anos
-		teste = gerClientes.validaIdade(cliente003.getIdade());
+
+		int idade65 = cliente003.getIdade();
+		teste = gerClientes.validaIdade(idade65);
 		assertTrue(teste);
 	}	
 	@Test
 	public void testeValidaIdade_menorQ18(){
-		cliente003.setIdade(17);
+		int idade17 = 17;
 		try {
-			teste = gerClientes.validaIdade(cliente003.getIdade());
+			teste = gerClientes.validaIdade(idade17);
 			fail();
 		} catch (Exception e) {
 			assertThat(e.getMessage(), is(IdadeNaoPermitidaException.MSG_IDADE_INVALIDA));
@@ -136,9 +143,9 @@ public class GerenciadoraClientesTest {
 	}
 	@Test
 	public void testeValidaIdade_maiorQ65(){
-		cliente003.setIdade(66);
+		int idade66 = 66;
 		try {
-			teste = gerClientes.validaIdade(cliente003.getIdade());
+			teste = gerClientes.validaIdade(idade66);
 			fail();
 		} catch (Exception e) {
 			assertThat(e.getMessage(), is(IdadeNaoPermitidaException.MSG_IDADE_INVALIDA));
